2757465915262b8ec5e2680c3540039a6a67c43c
Scott Clark <scottclark11@gmail.com>
2014-05-30 09:43:02 -0400

    enable Coveralls

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,14 @@
+require 'coveralls'
+Coveralls.wear_merged!
+
 require 'rspec/expectations'
 require 'test_config'
 
-Before do
-  ENV.delete('TEST_ENV')
-  TestConfig.configure!
-end
+# After do
+#   if File.file?('test_config_options.yml')
+#     File.delete('test_config_options.yml')
+#   end
+
+#   ENV.delete('TEST_ENV')
+#   TestConfig.configure!
+# end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,4 @@
+require 'coveralls'
+Coveralls.wear_merged!
+
 require 'test_config'
