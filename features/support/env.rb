772038075008cafd03cb790bd5fef2d8ce149f53
Scott Clark <scottclark11@gmail.com>
2014-07-14 19:25:17 -0400

    remove cucumber tests, for now...

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,12 +3,3 @@ Coveralls.wear_merged!
 
 require 'rspec/expectations'
 require 'test_config'
-
-# After do
-#   if File.file?('test_config_options.yml')
-#     File.delete('test_config_options.yml')
-#   end
-
-#   ENV.delete('TEST_ENV')
-#   TestConfig.configure!
-# end
